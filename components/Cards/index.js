be087a597010a99434cf71e56c6cc2951655973f
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-13 13:14:42 -0400

    Finally figured out how to get the data I need.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,33 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    // console.log(response);
+    // console.log(response.data.articles);
+    // const newTopic = response.data.articles;
+    const newTopic = Object.entries(response.data.articles);
+    // console.log(newTopic);
+    newTopic.forEach(topic => {
+      // console.log(topic); // An array containing the topic name and another array
+      // console.log(topic[0]); // Topic Name
+      // console.log(topic[1]); // An Array of data
+      topic[1].forEach(item => {
+        // console.log(item);
+        console.log(item.headline);
+        console.log(item.authorPhoto);
+        console.log(item.authorName);
+      })
+
+    })
+    // console.log(newTopic[0]);
+    // console.log(newTopic.length);
+    console.log(newTopic[0][1][0].headline);
+    // newTopic.forEach(topic => {
+    //   console.log(topic[0]);
+    // })
+  })
+  .catch(error => {
+    console.log("the data was not returned", error);
+  })
